dd944683007bf7d36ce8be074d506f2e53e7a680
ducthien94 <khuatducthien.94@gmail.com>
2020-04-22 14:30:38 +0700

    Updates

---
+++ precache-manifest.55c464524746adca885fc445320b918b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11ffec951b9702ca74090cf0703e4cef",
+    "url": "/ducthien94/ReactJS-App-QLND/index.html"
+  },
+  {
+    "revision": "f61c38bcddb90ca6bddb",
+    "url": "/ducthien94/ReactJS-App-QLND/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "8962b6d5230e85ab0b4b",
+    "url": "/ducthien94/ReactJS-App-QLND/static/css/main.7b646d04.chunk.css"
+  },
+  {
+    "revision": "f61c38bcddb90ca6bddb",
+    "url": "/ducthien94/ReactJS-App-QLND/static/js/2.cfba19d7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ducthien94/ReactJS-App-QLND/static/js/2.cfba19d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8962b6d5230e85ab0b4b",
+    "url": "/ducthien94/ReactJS-App-QLND/static/js/main.ffc4f2d7.chunk.js"
+  },
+  {
+    "revision": "487eff4cfc71d8e88cd7",
+    "url": "/ducthien94/ReactJS-App-QLND/static/js/runtime-main.0fefeaef.js"
+  }
+]);
